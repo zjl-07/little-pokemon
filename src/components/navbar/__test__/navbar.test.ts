d7bf4c0a1731b29df15f2e3fa8eb2db063ae173e
emilda zhang <emilda.zhang@lemonilo.com>
2021-10-05 01:29:56 +0700

    feat: set up library-testing

---
+++ src/components/navbar/__test__/navbar.test.ts
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Navbar from '../index';
+
+test('renders learn react link', () => {
+	// const navbarComponent = render(Navbar);
+	// console.log(navbarComponent);
+	// const navbarTitle = navbarComponent.getByText('Where in the world ?');
+	// expect(navbarTitle).toBeInTheDocument();
+});

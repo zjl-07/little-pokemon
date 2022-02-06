a857ad2250dcadff83ed5619bf9ccb5bd7cd6c95
emilda zhang <emilda.zhang@lemonilo.com>
2022-02-06 17:50:40 +0700

    fix: fix webpack error

--- next.config.js
+++ next.config.js
@@ -2,21 +2,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-const webpack = require('webpack');
-const path = require('path');
-
-const { parsed: parsedEnv } = require('dotenv').config({
-	path: path.join(__dirname, '/.env'),
-});
-
 module.exports = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ['en-US', 'id'],
 		defaultLocale: 'id',
-	},
-	webpack(config) {
-		config.plugins.push(new webpack.EnvironmentPlugin(parsedEnv));
-		return config;
 	},
 };

51d27500ccb769910690e8d1bc60040ddf43d7ef
emilda zhang <emilda.zhang@lemonilo.com>
2022-02-06 18:04:27 +0700

    fix: pages only need to content default export

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,76 @@
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
+import Navbar from '@components/navbar';
+import usePokemon from '@utils/pokemon/usePokemon';
 
-const Home = () => {
-	const { push } = useRouter();
-	useEffect(() => {
-		push({
-			pathname: '/pokemon',
-		});
-	}, []);
-	return <div />;
+import {
+	PokemonContainer,
+	PokemonCard,
+	SeeDetailsButton,
+	Section,
+	FavouriteButton,
+	PokemonCardName,
+} from '@utils/pokemon/styles';
+
+import Pagination from '@components/pagination';
+import PokemonDetailsModal from '@components/pokemon-details-modal';
+
+const Pokemon = () => {
+	const {
+		isLoading,
+		isError,
+		pokemonList,
+		modalState,
+		favPokemon,
+		handleCloseModal,
+		addToFavourite,
+		handlePokemonListItemClicked,
+	} = usePokemon();
+
+	if (isLoading || !pokemonList.results.length)
+		return (
+			<Section>
+				<Navbar title={'Pokemon'} />
+				<div>isLoading...</div>
+			</Section>
+		);
+	if (isError)
+		return (
+			<Section>
+				<Navbar title={'Pokemon'} />
+				<div>Error...</div>
+			</Section>
+		);
+
+	return (
+		<Section>
+			<Navbar title={'Pokemon'} />
+			<PokemonContainer>
+				{pokemonList.results.map(({ name, url }: { name: string; url: string }) => (
+					<PokemonCard key={name}>
+						<PokemonCardName>
+							<FavouriteButton onClick={() => addToFavourite(name)}>
+								<img
+									src={`heart-_${favPokemon[name] ? 1 : 2}_.svg`}
+									width={20}
+									height={20}
+								></img>
+							</FavouriteButton>
+							{name}
+						</PokemonCardName>
+						<SeeDetailsButton onClick={() => handlePokemonListItemClicked(url)}>
+							See Details
+						</SeeDetailsButton>
+					</PokemonCard>
+				))}
+			</PokemonContainer>
+			<Pagination page={1} size={10} />
+			<PokemonDetailsModal
+				visible={modalState.modalVisible}
+				handleCloseModal={handleCloseModal}
+				data={modalState?.content}
+			/>
+		</Section>
+	);
 };
 
-export default Home;
+export default Pokemon;
